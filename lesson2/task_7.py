288a62776c3d4756cbb96dcbdadc7133fbe368e9
Kamilgeek <kamil-magomedov-00@mail.ru>
2019-09-20 06:29:07 +0400

    lesson 2.2

---
+++ lesson2/task_7.py
@@ -0,0 +1,14 @@
+def fact(n):
+    if n == 0:
+        return 1
+    return fact(n-1) * n
+
+
+
+def rav(n):
+    if fact(n) == n*(n+1)/2:
+        print('равенство выполняется!')
+    else:
+        print('равенство не выполняется!')
+
+rav(3)
